5d018a5664b1d3accb43af076f74d2380d056d6d
RyanDurling <ryan.p.durling@gmail.com>
2012-09-06 19:28:29 -0700

    Patu #7 - The Beginning

---
+++ src/test/java/features/patuTest.java
@@ -0,0 +1,28 @@
+package features;
+
+import cucumber.annotation.en.Given;
+import cucumber.annotation.en.Then;
+import cucumber.annotation.en.When;
+import cucumber.runtime.PendingException;
+
+public class patuTest {
+
+    @Given("^Patu is running$")
+    public void Patu_is_running() throws Throwable {
+        // Express the Regexp above with the code you wish you had
+        throw new PendingException();
+    }
+
+    @When("^I connect to port '(\\d+)'$")
+    public void I_connect_to_port_(int arg1) throws Throwable {
+        // Express the Regexp above with the code you wish you had
+        throw new PendingException();
+    }
+
+    @Then("^The connection is 'accepted'$")
+    public void The_connection_is_accepted() throws Throwable {
+        // Express the Regexp above with the code you wish you had
+        throw new PendingException();
+    }
+
+}
